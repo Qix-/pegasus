adead9bae5a471246242d2bca9459ccce967028e
Josh Junon <junon@uber.com>
2016-10-04 07:09:45 -0700

    added max depth argument to flatten() util

--- pegasus/util.py
+++ pegasus/util.py
@@ -1,10 +1,13 @@
 """A few utilities"""
 
 
-def flatten(obj):
-    for o in obj:
-        if type(o) in [tuple, list]:
-            for o2 in flatten(o):
-                yield o2
-        else:
-            yield o
+def flatten(obj, depth=None, current_depth=0):
+    if depth is not None and current_depth > depth:
+        yield obj
+    else:
+        for o in obj:
+            if type(o) in [tuple, list]:
+                for o2 in flatten(o, depth, current_depth + 1):
+                    yield o2
+            else:
+                yield o
