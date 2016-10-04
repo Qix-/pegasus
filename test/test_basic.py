72897ff97d4f3a1511fcd2539a25741aa9758275
Josh Junon <junon@uber.com>
2016-10-04 04:05:08 -0700

    fix basic test to reflect newest calling convention

--- test/test_basic.py
+++ test/test_basic.py
@@ -7,23 +7,23 @@ from pegasus.rules import Plus, Opt, Discard, Star, ChrRange as C, EOF, Str
 
 class SimpleParser(Parser):
     @rule(['hello', 'greetings', 'salutations', 'yo'])
-    def greeting(self, _):
+    def greeting(self, *_):
         pass
 
     @rule(Str(Plus([C['a':'z'], C['A':'Z']])))
-    def name(self, (name,)):
+    def name(self, name):
         return name
 
     @rule(Discard(greeting, Opt(','), Plus(' ')), name, Discard(Star('!')), EOF)
-    def hello_world(self, (name,)):
+    def hello_world(self, name):
         return name
 
 
 def test_simple_parser():
     parser = SimpleParser()
-    assert 'Paul' == parser.parse(SimpleParser.hello_world, 'hello, Paul!')[0]
-    assert 'Sheila' == parser.parse(SimpleParser.hello_world, 'hello,     Sheila')[0]
-    assert 'Josh' == parser.parse(SimpleParser.hello_world, 'hello,     Josh!!!')[0]
-    assert 'Paul' == parser.parse(SimpleParser.hello_world, 'greetings, Paul!')[0]
-    assert 'Sheila' == parser.parse(SimpleParser.hello_world, 'yo,   Sheila!')[0]
-    assert 'Josh' == parser.parse(SimpleParser.hello_world, 'salutations,     Josh')[0]
+    assert 'Paul' == parser.parse(SimpleParser.hello_world, 'hello, Paul!')
+    assert 'Sheila' == parser.parse(SimpleParser.hello_world, 'hello,     Sheila')
+    assert 'Josh' == parser.parse(SimpleParser.hello_world, 'hello,     Josh!!!')
+    assert 'Paul' == parser.parse(SimpleParser.hello_world, 'greetings, Paul!')
+    assert 'Sheila' == parser.parse(SimpleParser.hello_world, 'yo,   Sheila!')
+    assert 'Josh' == parser.parse(SimpleParser.hello_world, 'salutations,     Josh')
