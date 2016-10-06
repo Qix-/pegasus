9dc233e8b33cf9e1ef22bba22237c87f9f53fd9e
Josh Junon <junon@uber.com>
2016-10-06 02:59:32 -0700

    0.1.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='pegasus',
     packages=['pegasus'],
-    version='0.1.0',
+    version='0.1.1',
     description='Yet another PEG parser generator',
     author='Josh Junon',
     author_email='josh@junon.me',
     url='https://github.com/qix-/pegasus',
-    download_url='https://github.com/qix-/pegasus/tarball/0.1.0',
+    download_url='https://github.com/qix-/pegasus/tarball/0.1.1',
     keywords=['peg', 'packrat', 'pakrat', 'parser', 'generator', 'pegasus', 'visitor', 'expression', 'grammar'],
     classifiers=[
         'Development Status :: 3 - Alpha',
